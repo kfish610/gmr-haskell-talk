58f56b661db5ac53c0ca8d8c77aee5bbe87471a2
Kevin Fisher <kfish610@gmail.com>
2021-04-09 10:52:39 -0700

    Improve Zoo

--- src/Zoo.hs
+++ src/Zoo.hs
@@ -2,21 +2,21 @@ module Zoo where
 
 data Animal = DefineThis
 
---Either "Meow" or "... Giraffes don't make sounds"
+-- Either "Meow" or "..."
 makeSound :: Animal -> String
 makeSound = undefined
 
---giraffes don't have names
-getName :: Animal -> String
+-- Giraffes don't have names
+getName :: Animal -> Maybe String
 getName = undefined
 
---Increase chonkiness or neck length by the amount inputted
+-- Increase chonkiness or neck length by the amount inputted
 feed :: Animal -> Int -> Animal
 feed = undefined
 
---This is a battle zoo! 
---The battle function takes two animals and returns the one who would win in a fight.
---Giraffes always beat cats.
---If they are both giraffes or cats, the winner is the one with the least chonkiness or the longest neck.
+-- This is a battle zoo!
+-- The battle function takes two animals and returns the one who would win in a fight.
+-- Giraffes always beat cats.
+-- If they are both giraffes or cats, the winner is the one with the least chonkiness or the longest neck.
 battle :: Animal -> Animal -> Animal
 battle = undefined
